89681ac1e59130f83952f31c2040d444e5a9f355
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2025-07-13 22:39:23 -0400

    get utf8 codepoint size

--- src/utils/toml.c
+++ src/utils/toml.c
@@ -1,9 +1,17 @@
 #include "utils/types.h"
 #include "utils/toml.h"
 
+static const u32 utf8_sizes[] = {
+    1, 1, 1, 1,
+    1, 1, 1, 1,
+    0, 0, 0, 0,
+    2, 2, 3, 4
+};
+
 static gl_pos pos_init(void);
 static gl_pos pos_w_next_line(const gl_pos* _pos, u32 _bytes);
 static gl_pos pos_w_next_col(const gl_pos* _pos, u32 _bytes);
+static u32 utf8_r_size(const u8* _data);
 
 
 static gl_pos pos_init(void) {
@@ -27,4 +35,9 @@ static gl_pos pos_w_next_col(const gl_pos* _pos, u32 _bytes) {
     pos.col++;
     pos.index += _bytes;
     return pos;
+}
+
+static u32 utf8_r_size(const u8* _data) {
+    const u32 index = (_data[0] & 0xff) >> 4;
+    return utf8_sizes[index];
 }
