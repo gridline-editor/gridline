1d49de46cbfb97767ae4bd16eba29d8bd6b76188
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2025-07-25 21:51:22 -0400

    fixed eof token formatting for newlines

--- src/main.c
+++ src/main.c
@@ -4,21 +4,32 @@
 #include "utils/toml.h"
 
 int main(void) {
-    char* data = "\"\\u11ab \\U00033a1\"";
+    char* data = "\"\\u11ab\"\n \'hello\'\n";
     printf("size: %zu\n", strlen(data));
     gl_source source = gl_source_init(0, (u8*) data, strlen(data));
     gl_toml_lexer lexer = gl_toml_lexer_init(&source);
     u32 last_index = 0;
+    u32 cp = 0;
 
-    do {
+    while(1) {
         last_index = lexer.pos.index;
         lexer = gl_toml_lexer_lex(&lexer);
-        printf("\'%c\' (%u:%u): %u\n",
-               source.data[lexer.token_pos.index],
-               lexer.token_pos.ln,
-               lexer.token_pos.col,
-               lexer.first_nonblank.col);
-    } while(lexer.pos.index != last_index);
+        cp = source.data[lexer.token_pos.index];
+        if(cp == '\n') {
+            printf("\'\\n\' (%u:%u)\n",
+                   lexer.token_pos.ln,
+                   lexer.token_pos.col);
+        } else {
+            printf("\'%c\'  (%u:%u)\n",
+                   cp,
+                   lexer.token_pos.ln,
+                   lexer.token_pos.col);
+        }
+
+        if(lexer.pos.index == last_index) {
+            break;
+        }
+    }
 
     printf("end: %u\n", lexer.pos.index);
     return 0;
