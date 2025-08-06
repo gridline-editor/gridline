a967439e29a0788e7ba512e11a3930d06b3bc395
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2025-08-05 20:43:14 -0400

    built compile db

--- build.zig
+++ build.zig
@@ -1,28 +1,13 @@
 const std = @import("std");
 
-const c_flags = [_][]const u8{
-    "-std=c11",
-    "-O0",
-    "-g",
-    "-Wall",
-    "-Wextra",
-    "-pedantic",
-    "-Wvla",
-    "-Wc++-compat",
-};
-
-const c_source_independent = [_][]const u8{
+const c_sources = [_][]const u8{
     "src/main.c",
     "src/utils/memory.c",
     "src/utils/stack_allocator.c",
     "src/utils/toml.c",
 };
 
-const c_include_paths = [_][]const u8{
-    "src",
-};
-
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -33,11 +18,46 @@ pub fn build(b: *std.Build) void {
         .link_libc = true,
     });
 
-    exe.addCSourceFiles(.{
-        .files = &c_source_independent,
-        .flags = &c_flags,
-    });
-
     exe.addIncludePath(b.path("src"));
+
+    const allocator = b.allocator;
+    var db_pathnames = std.ArrayList([]const u8).init(allocator);
+    defer db_pathnames.deinit();
+
+    for (c_sources) |filename| {
+        const basename = std.fs.path.basename(filename);
+        const db_path = try std.fs.path.join(allocator, &.{
+            b.cache_root.path.?,
+            try std.fmt.allocPrint(allocator, "{s}.json", .{basename}),
+        });
+        try db_pathnames.append(db_path);
+
+        exe.addCSourceFile(.{
+            .file = b.path(filename),
+            .flags = &.{
+                "-std=c11",
+                "-O0",
+                "-g",
+                "-Wall",
+                "-Wextra",
+                "-pedantic",
+                "-Wvla",
+                "-Wc++-compat",
+                "-MJ",
+                db_path,
+            },
+        });
+    }
+
     b.installArtifact(exe);
+
+    const sed_cmd_str = try std.fmt.allocPrint(
+        allocator,
+        "sed -e '1s/^/[/' -e '$s/,$/]/' {s}/*.json > compile_commands.json",
+        .{b.cache_root.path.?},
+    );
+
+    const sed_step = b.addSystemCommand(&.{ "sh", "-c", sed_cmd_str });
+    const merge_step = b.step("compile-db", "Generate compile_commands.json");
+    merge_step.dependOn(&sed_step.step);
 }
