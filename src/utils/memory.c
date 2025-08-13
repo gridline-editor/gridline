00fe5cac039340addeb9f4c4b0c932c8073e2a15
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2025-08-13 08:26:36 -0400

    fixed endianness for serialize_u32() to LE

--- src/utils/memory.c
+++ src/utils/memory.c
@@ -11,10 +11,11 @@ u32 deserialize_u32(const u8* _data, u32 _bytes) {
 }
 
 void serialize_u32(u8* _data, u32 _value) {
-    _data[0] = (_value >> 24) & 0xff;
-    _data[1] = (_value >> 16) & 0xff;
-    _data[2] = (_value >> 8) & 0xff;
-   _data[3] = _value & 0xff;
+   _data[0] = _value & 0xff;
+    _data[1] = (_value >> 8) & 0xff;
+    _data[2] = (_value >> 16) & 0xff;
+    _data[3] = (_value >> 24) & 0xff;
+}
 
 u32 align_up_8_u32(u32 _n) {
     return ((_n + 7) & ~(7));
